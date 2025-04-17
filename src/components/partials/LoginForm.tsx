6fb999104b7d22aabd9e1896d0a42e584c27f488
truegreatvoid <andrade.tiagoxavier@outlook.com>
2025-04-17 10:32:15 -0300

    Atualizado em 17-04-2025 10:32, por Tiago Xavier [Notebook]

--- src/components/partials/LoginForm.tsx
+++ src/components/partials/LoginForm.tsx
@@ -17,7 +17,7 @@ export function LoginForm() {
     setError("");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/login/", {
+      const res = await fetch("http://172.20.0.4:8000/api/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
